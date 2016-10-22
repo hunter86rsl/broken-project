2fb99f9ce22cb10ff95d53f817018e30469cec6c
Adrian <hunter86@onet.eu>
2016-10-22 11:03:39 +0200

    Init broken project

---
+++ src/main/java/com/owczarek/BrokenProjectApplication.java
@@ -0,0 +1,15 @@
+package com.owczarek;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrokenProjectApplication{
+	@Autowired
+	SomeService someService;
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrokenProjectApplication.class, args);
+	}
+}
---
+++ src/main/java/com/owczarek/SomeService.java
@@ -0,0 +1,7 @@
+package com.owczarek;
+
+/**
+ * Created by Adrian on 2016-10-22.
+ */
+public interface SomeService {
+}
